51bc6999775029e7e5ca6121e58e8f890778904d
XlebDaMaslo <prof.fatop@gmail.com>
2024-09-11 22:14:16 +0700

    Completed task 2 lab1

--- ОСМС/lab1.py
+++ ОСМС/lab1.py
@@ -1,9 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fft import fft, fftfreq
 
-t = np.arange(0, 2.001, 0.0001)
+#1
+t_lim = 2
+fd = 1000
+t = np.arange(0, t_lim+(1/fd), (1/fd))
+print(t)
+f = 4
 
-y = np.sin((12 * np.pi * 4 * t) + (np.pi / 11)) + np.sin(10 * np.pi * 4 * t)
+y = np.sin((12 * np.pi * f * t) + (np.pi / 11)) + np.sin(10 * np.pi * f * t)
+
 
 plt.plot(t, y)
 plt.xlabel('Время (s)')
@@ -11,3 +18,18 @@ plt.ylabel('Амплитуда')
 plt.title('График сигнала')
 plt.grid(True)
 plt.show()
+
+#2
+spectr = fft(y)
+f_arr = fftfreq(len(t), 1/fd)
+a_spectr = np.abs(spectr) / len(t)
+
+positive_fs = f_arr[:len(f_arr)//2]
+positive_spectr = a_spectr[:len(a_spectr)//2]
+
+max_idx = np.argmax(positive_spectr)
+max_f = positive_fs[max_idx]
+
+print(f'Максимальная частота в спектре: {round(max_f, 6)} Hz')
+
+#3
