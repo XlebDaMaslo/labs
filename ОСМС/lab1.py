964f7941ff05dad425b2ed38d6632df85f0fb0de
XlebDaMaslo <prof.fatop@gmail.com>
2024-09-12 21:40:22 +0700

    Completed task 4 lab1

--- ОСМС/lab1.py
+++ ОСМС/lab1.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 from scipy.fft import fft, fftfreq
 
 #1
-t_lim = 2
+t_lim = 1
 n = 1000
-t = np.arange(0, t_lim+(1/n), (1/n))
+#t = np.arange(0, t_lim+(1/n), (1/n))
+t = np.linspace(0, t_lim, n, endpoint=False)
 f = 4
 
 y = np.sin((12 * np.pi * f * t) + (np.pi / 11)) + np.sin(10 * np.pi * f * t)
@@ -34,3 +35,24 @@ print(f'Максимальная частота в спектре: {round(max_f,
 min_fd = 2 * max_f
 
 print(f"Минимальная частота дискретизации: {round(min_fd,6)} Гц")
+
+#4
+sampling_frequency = min_fd
+
+num_samples = int(sampling_frequency * t_lim)
+
+t_digitized = np.linspace(0, t_lim, num_samples, endpoint=False)
+
+y_digitized = np.sin((12 * np.pi * f * t_digitized) + (np.pi / 11)) + np.sin(10 * np.pi * f * t_digitized)
+
+digitized_signal = y_digitized
+
+plt.stem(t_digitized, y_digitized)
+plt.xlabel('Время (s)')
+plt.ylabel('Амплитуда')
+plt.title('Оцифрованный сигнал')
+plt.grid(True)
+plt.show()
+
+print("Оцифрованный сигнал:")
+print(digitized_signal)
