1848ec4716555d480b2d454f7642331048a79366
XlebDaMaslo <prof.fatop@gmail.com>
2024-09-17 11:11:06 +0700

    Complete lab1 MathDSP

--- MathDSP/MathDSPlab1_3.py
+++ MathDSP/MathDSPlab1_3.py
@@ -1,24 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-A1 = 8
+f = 3
+A1 = 2
+phi1 = np.pi/2
 A2 = 5
-f = 5
-d = 1
-fd = 1000
-t = np.linspace(0, d, int(d * fd))
+phi2 = 3*np.pi/4
+A3 = 0.5
+phi3 = 5*np.pi/3
 
-ph1 = np.pi/3
-ph2 = (4*np.pi)/3
+t = np.linspace(0, 2, 1000)
 
-x1 = A1*np.sin(2 * np.pi * f * t + ph1)
-x2 = A2*np.sin(2 * np.pi * f * t + ph2)
-x3 = x1 + x2
+x1 = A1 * np.cos(2 * np.pi * f * t + phi1)
+x2 = A2 * np.cos(2 * np.pi * f * t + phi2)
+x3 = A3 * np.cos(2 * np.pi * f * t + phi3)
 
+x_sum = x1 + x2 + x3
 
-plt.plot(t,x3)
-plt.xlabel("Time")
-plt.ylabel("Amplitude")
-plt.title("A1={}V, A2={}V ,F={} Hz, $\phi1={}^\circ$, $\phi2={}^\circ$".format(A1,A2,f,ph1,ph2))
-
+plt.plot(t, x1, label='x1')
+plt.plot(t, x2, label='x2')
+plt.plot(t, x3, label='x3')
+plt.plot(t, x_sum, label='x1 + x2 + x3')
+plt.xlabel('Время')
+plt.ylabel('Амплитуда')
+plt.legend()
+plt.grid(True)
 plt.show()
---
+++ MathDSP/MathDSPlab1_4.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = 1
+A1 = 4 / np.pi
+A2 = 4 / (3 * np.pi)
+phi1 = -np.pi / 2
+phi2 = -np.pi / 2
+
+t = np.linspace(0, 5, 1000)
+
+x = A1 * np.cos(2 * np.pi * f * t + phi1) + A2 * np.cos(2 * np.pi * 3 * f * t + phi2)
+
+plt.plot(t, x)
+plt.title('Результирующее колебание')
+plt.xlabel('Время (с)')
+plt.ylabel('Амплитуда')
+plt.grid(True)
+plt.show()
+
+f = 0.25  # Основная частота, Гц
+N = 5
+t = np.linspace(0, 5, 1000)
+
+x_total = np.zeros_like(t)
+
+plt.figure(figsize=(10, 8))
+
+n=0
+for n in range(N+1):
+    harmonic = (4 / ((2*n - 1) * np.pi)) * np.cos(2 * np.pi * (2*n - 1) * f * t - np.pi / 2)
+    
+    x_total += harmonic
+    n+=1
+    
+    plt.plot(t, x_total, label=f'Слагаемых: {n}')
+
+plt.title('Результирующее колебание при увеличении числа гармоник')
+plt.xlabel('Время (с)')
+plt.ylabel('Амплитуда')
+plt.grid(True)
+plt.legend()
+plt.show()
