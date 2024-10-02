c334b3ba1ddcbfdd0db4741ea8abd95ff4ccfe4c
XlebDaMaslo <prof.fatop@gmail.com>
2024-10-03 01:03:24 +0700

    Complete lab2 OCMC

--- ОСМС/lab2.py
+++ ОСМС/lab2.py
@@ -18,14 +18,12 @@ NoiseFigureBS = 2.4 # дБ Коэффициент шума приемника BS
 NoiseFigureUE = 6 # дБ Коэффициент шума приемника пользователя
 RequiredSINR_DL = 2 # дБ Требуемое отношение SINR для DL
 RequiredSINR_UL = 4 # дБ Требуемое отношение SINR для UL
-NumTransRecAnt_BS = 12 # Число приемо-передающих антенн на BS (MIMO)
+MIMOGain = 3 # дБ Выигрыш за счет использования MIMO (Число приемо-передающих антенн на BS (MIMO): 2)
 AreaTerritory = 100 # кв.км Площадь территории, на которой требуется спроектировать сеть
 AreaShoppingBusiness = 4 # кв.км Площадь торговых и бизнес центров, где требуется спроектировать сеть на базе микро- и фемтосот
-# Базовые станции с фидерами
-MIMOGain = 3
-FeederLoss = 2 # дБ
+FeederLoss = 2 # дБ Уровень потерь сигнала при прохождении через фидер или джампер (Базовые станции с фидерами)
 
-#1
+#1 Расчет уровня максимально допустимых потерь в восходящем канале
 ThermalNoise = -174 + 10*np.log10(BW_UL)
 RxSensBS = NoiseFigureBS + ThermalNoise + RequiredSINR_UL
 
@@ -33,14 +31,14 @@ MAPL_UL = TxPowerUE - FeederLoss + AntGainBS + MIMOGain - IM - PenetrationM - Rx
 
 print(f"MAPL_UL: {MAPL_UL:.4f} дБ")
 
-#2
+#2 Расчет уровня максимально допустимых потерь в нисходящем канале
 ThermalNoise = -174 + 10*np.log10(BW_DL)
 RxSensUE = NoiseFigureUE + ThermalNoise + RequiredSINR_DL
 
 MAPL_DL = TxPowerBS - FeederLoss + AntGainBS + MIMOGain - IM - PenetrationM - RxSensUE
 print(f"MAPL_DL: {MAPL_DL:.4f} дБ")
 
-#3 Dense Urban (плотная городская застройка)
+#3 Построение моделей распространения сигнала
 d = np.linspace(10, 1000, 1000)
 A = 69.55 if (f >= 150 and f <= 1500) else 46.3 if (f >= 1500 and f <= 2000) else 0
 B = 26.16 if (f >= 150 and f <= 1500) else 33.9 if (f >= 1500 and f <= 2000) else 0
@@ -54,7 +52,7 @@ phi = 45
 # UMiNLOS(Urban Micro Non-Line-of-Sight)
 PL_UMiNLOS = 26 * np.log10(f) + 22.7 + 36.7 * np.log10(d)
 
-# COST231(Окумура-Хата)
+# COST231(Окумура-Хата) DU
 a_hms = 3.2 * (np.log10(11.75 * hms))**2 - 4.97
 Lclutter = 3
 s = 44.9 - 6.55 * np.log10(f)
@@ -75,7 +73,7 @@ Kf = -4 + 0.7 * (f/925 - 1)
 L1 = L11 + Ka + Kd * np.log10(d) + Kf * np.log10(f) - 9 * np.log10(b)
 PL_Walfish_NLOS = np.where((L1 + L2) <= 0, L0, L0 + L1 + L2)
 
-plt.figure(figsize=(15, 10))
+plt.figure(figsize=(10, 5))
 plt.plot(d, PL_UMiNLOS, label="UMiNLOS")
 plt.plot(d, PL_COST231, label="COST231(DU)")
 plt.plot(d, PL_Walfish_LOS, label="Walfish-Ikegami (LOS)")
@@ -90,7 +88,7 @@ plt.grid(True)
 plt.ylim(0, 250)
 plt.show()
 
-#4
+#4 Определение радиусов, площадей и количество базовых станций для макросот и микросот
 d_UL_Macro = d[np.where(PL_COST231 <= MAPL_UL)[0][-1]] if np.any(PL_COST231 <= MAPL_UL) else 0
 d_DL_Macro = d[np.where(PL_COST231 <= MAPL_DL)[0][-1]] if np.any(PL_COST231 <= MAPL_DL) else 0
 
@@ -118,4 +116,3 @@ S_Femto = 1.95 * R_Femto**2
 print(f"Площадь фемто- и микросоты: {S_Femto:.4f} кв.м")
 NumBS_Femto = np.ceil((AreaShoppingBusiness*1000000) / S_Femto)
 print(f"Необходимое количество базовых станций (сайтов): {NumBS_Femto}")
-
