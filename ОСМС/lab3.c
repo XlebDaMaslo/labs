71ed897b26ecee1768094b585c7aaa3208db5d30
XlebDaMaslo <prof.fatop@gmail.com>
2024-10-09 22:00:41 +0700

    Task 1 lab3 OCMC

--- ОСМС/lab3.c
+++ ОСМС/lab3.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
 #include <math.h>
 
-float correlation(float arr1[], float arr2[], int n) {
-    float sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += arr1[i] * arr2[i];
+double correlation(int a[], int b[], int N) {
+    double result = 0;
+    for (int i = 0; i < N; i++) {
+        result += a[i] * b[i];
     }
-    return sum;
+    return result;
 }
 
-float normalized_correlation(float arr1[], float arr2[], int n) {
-    float sum_xy = 0, sum_x = 0, sum_y = 0, sum_x2 = 0, sum_y2 = 0;
-
-    for (int i = 0; i < n; i++) {
-        sum_xy += arr1[i] * arr2[i];
-        sum_x += arr1[i];
-        sum_y += arr2[i];
-        sum_x2 += arr1[i] * arr1[i];
-        sum_y2 += arr2[i] * arr2[i];
+double normalized_correlation(int a[], int b[], int N) {
+    double numerator = 0, sum_a = 0, sum_b = 0;
+    for (int i = 0; i < N; i++) {
+        numerator += a[i] * b[i];
+        sum_a += a[i] * a[i];
+        sum_b += b[i] * b[i];
     }
-  
-   return sum_xy / (sqrt(sum_x2) * sqrt(sum_y2));
+    return numerator / sqrt(sum_a * sum_b);
 }
-
 
 int main() {
-    float a[] = {6, 2, 3, -2, -4, -4, 1, 1};
-    float b[] = {3, 1, 5, 0, -3, -4, 2, 3};
-    float c[] = {-1, -1, 3, -9, 2, -8, 4, -4};
-    int n = sizeof(a) / sizeof(a[0]);
+    int a[] = {6, 2, 3, -2, -4, -4, 1, 1};
+    int b[] = {3, 1, 5, 0, -3, -4, 2, 3};
+    int c[] = {-1, -1, 3, -9, 2, -8, 4, -4};
+    int N = 8;
 
-    printf("Корреляция:\n");
-    printf("a и b: %f\n", correlation(a, b, n));
-    printf("a и c: %f\n", correlation(a, c, n));
-    printf("b и c: %f\n", correlation(b, c, n));
+    double corr_ab = correlation(a, b, N);
+    double corr_ac = correlation(a, c, N);
+    double corr_bc = correlation(b, c, N);
+    
+    printf("Корреляция a и b: %f\n", corr_ab);
+    printf("Корреляция a и c: %f\n", corr_ac);
+    printf("Корреляция b и c: %f\n", corr_bc);
 
-    printf("\nНормализованная корреляция:\n");
-    printf("a и b: %f\n", normalized_correlation(a, b, n));
-    printf("a и c: %f\n", normalized_correlation(a, c, n));
-    printf("b и c: %f\n", normalized_correlation(b, c, n));
+    double norm_corr_ab = normalized_correlation(a, b, N);
+    double norm_corr_ac = normalized_correlation(a, c, N);
+    double norm_corr_bc = normalized_correlation(b, c, N);
+
+    printf("Нормализованная корреляция a и b: %f\n", norm_corr_ab);
+    printf("Нормализованная корреляция a и c: %f\n", norm_corr_ac);
+    printf("Нормализованная корреляция b и c: %f\n", norm_corr_bc);
 
     return 0;
 }
