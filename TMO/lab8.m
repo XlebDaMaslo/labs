f48ef68db01a94fff9730e9f1bbf8c6d8c25a433
XlebDaMaslo <prof.fatop@gmail.com>
2024-11-06 14:02:49 +0700

    Fix labs TMO

--- TMO/lab8.m
+++ TMO/lab8.m
@@ -1,146 +1,151 @@
-% Задаем параметры
+% Основные параметры
 lambda = 1; % интенсивность потока заявок (например, 1 заявка в единицу времени)
 x_mean = 5; % среднее время обслуживания (например, 5 единиц времени)
 mu = 1 / x_mean; % интенсивность обслуживания
+rho_vals = linspace(0.01, 0.99, 100); % коэффициент загрузки (0 < ρ < 1)
+C2b_vals = 0:10:100; % нормированная дисперсия обслуживания для M/G/1
 
-rho_vals = linspace(0.01, 0.99, 100); % Коэффициент загрузки (0 < ρ < 1)
-C2b_vals = 0:10:100; % Нормированная дисперсия обслуживания для M/G/1
+%% Функции для расчета характеристик
+% Функция для M/G/1
+function [Nq, N, W, T] = MG1_calculations(rho_vals, x_mean, C2b_vals)
+    Nq = zeros(length(C2b_vals), length(rho_vals));
+    N = zeros(length(C2b_vals), length(rho_vals));
+    W = zeros(length(C2b_vals), length(rho_vals));
+    T = zeros(length(C2b_vals), length(rho_vals));
 
-% Подготовка переменных для хранения результатов
-Nq_MG1 = zeros(length(C2b_vals), length(rho_vals));
-N_MG1 = zeros(length(C2b_vals), length(rho_vals));
-W_MG1 = zeros(length(C2b_vals), length(rho_vals));
-T_MG1 = zeros(length(C2b_vals), length(rho_vals));
-
-Nq_MD1 = zeros(1, length(rho_vals));
-N_MD1 = zeros(1, length(rho_vals));
-W_MD1 = zeros(1, length(rho_vals));
-T_MD1 = zeros(1, length(rho_vals));
-
-Nq_MM1 = zeros(1, length(rho_vals));
-N_MM1 = zeros(1, length(rho_vals));
-W_MM1 = zeros(1, length(rho_vals));
-T_MM1 = zeros(1, length(rho_vals));
-
-%% 2. Характеристики для M/G/1 (формулы 7.1-7.4)
-for i = 1:length(C2b_vals)
-    C2b = C2b_vals(i);
-    for j = 1:length(rho_vals)
-        rho = rho_vals(j);
-        
-        % Средняя длина очереди (формула 7.1)
-        Nq_MG1(i,j) = (rho^2 * (1 + C2b)) / (2 * (1 - rho));
-        
-        % Среднее число заявок в СМО (формула 7.2)
-        N_MG1(i,j) = rho + Nq_MG1(i,j);
-        
-        % Среднее время ожидания (формула 7.3)
-        W_MG1(i,j) = (rho * x_mean * (1 + C2b)) / (2 * (1 - rho));
-        
-        % Среднее время пребывания заявки в системе (формула 7.4)
-        T_MG1(i,j) = x_mean + W_MG1(i,j);
+    for i = 1:length(C2b_vals)
+        C2b = C2b_vals(i);
+        for j = 1:length(rho_vals)
+            rho = rho_vals(j);
+            if rho >= 1
+                warning('Значение \rho слишком близко к 1. Пропускаем расчет.');
+                continue;
+            end
+            % Вычисления для M/G/1
+            Nq(i,j) = (rho^2 * (1 + C2b)) / (2 * (1 - rho));
+            N(i,j) = rho + Nq(i,j);
+            W(i,j) = (rho * x_mean * (1 + C2b)) / (2 * (1 - rho));
+            T(i,j) = x_mean + W(i,j);
+        end
     end
 end
 
-%% 3. Характеристики для M/D/1 (формулы 7.5-7.8)
-for j = 1:length(rho_vals)
-    rho = rho_vals(j);
+% Функция для M/D/1
+function [Nq, N, W, T] = MD1_calculations(rho_vals, x_mean)
+    Nq = zeros(1, length(rho_vals));
+    N = zeros(1, length(rho_vals));
+    W = zeros(1, length(rho_vals));
+    T = zeros(1, length(rho_vals));
     
-    % Средняя длина очереди (формула 7.5)
-    Nq_MD1(j) = rho^2 / (2 * (1 - rho));
-    
-    % Среднее число заявок в СМО (формула 7.6)
-    N_MD1(j) = Nq_MD1(j) + rho;
-    
-    % Среднее время ожидания (формула 7.7)
-    W_MD1(j) = rho * x_mean / (2 * (1 - rho));
-    
-    % Среднее время пребывания заявки в системе (формула 7.8)
-    T_MD1(j) = W_MD1(j) + x_mean;
+    for j = 1:length(rho_vals)
+        rho = rho_vals(j);
+        if rho >= 1
+            warning('Значение \rho слишком близко к 1. Пропускаем расчет.');
+            continue;
+        end
+        % Вычисления для M/D/1
+        Nq(j) = rho^2 / (2 * (1 - rho));
+        N(j) = Nq(j) + rho;
+        W(j) = rho * x_mean / (2 * (1 - rho));
+        T(j) = W(j) + x_mean;
+    end
 end
 
-%% 4. Характеристики для M/M/1 (формулы 7.9-7.12)
-for j = 1:length(rho_vals)
-    rho = rho_vals(j);
+% Функция для M/M/1
+function [Nq, N, W, T] = MM1_calculations(rho_vals, x_mean)
+    Nq = zeros(1, length(rho_vals));
+    N = zeros(1, length(rho_vals));
+    W = zeros(1, length(rho_vals));
+    T = zeros(1, length(rho_vals));
     
-    % Средняя длина очереди (формула 7.9)
-    Nq_MM1(j) = rho^2 / (1 - rho);
-    
-    % Среднее число заявок в СМО (формула 7.10)
-    N_MM1(j) = rho / (1 - rho);
-    
-    % Среднее время ожидания (формула 7.11)
-    W_MM1(j) = rho * x_mean / (1 - rho);
-    
-    % Среднее время пребывания заявки в системе (формула 7.12)
-    T_MM1(j) = W_MM1(j) + x_mean;
+    for j = 1:length(rho_vals)
+        rho = rho_vals(j);
+        if rho >= 1
+            warning('Значение \rho слишком близко к 1. Пропускаем расчет.');
+            continue;
+        end
+        % Вычисления для M/M/1
+        Nq(j) = rho^2 / (1 - rho);
+        N(j) = rho / (1 - rho);
+        W(j) = rho * x_mean / (1 - rho);
+        T(j) = W(j) + x_mean;
+    end
 end
 
-%% 5. Построение графиков
-% M/G/1
-figure;
-for i = 1:length(C2b_vals)
+%% Функция для построения графиков
+function plot_results_MG1(rho_vals, Nq, N, W, T, C2b_vals)
+    figure;
+    
     subplot(2,2,1);
-    plot(rho_vals, Nq_MG1(i,:)); hold on;
+    for i = 1:length(C2b_vals)
+        plot(rho_vals, Nq(i,:)); hold on;
+    end
     title('N_q для M/G/1');
+    xlabel('\rho'); ylabel('N_q');
+    legend(arrayfun(@(x) ['C2b = ', num2str(x)], C2b_vals, 'UniformOutput', false));
+    
+    subplot(2,2,2);
+    for i = 1:length(C2b_vals)
+        plot(rho_vals, N(i,:)); hold on;
+    end
+    title('N для M/G/1');
+    xlabel('\rho'); ylabel('N');
+    legend(arrayfun(@(x) ['C2b = ', num2str(x)], C2b_vals, 'UniformOutput', false));
+    
+    subplot(2,2,3);
+    for i = 1:length(C2b_vals)
+        plot(rho_vals, W(i,:)); hold on;
+    end
+    title('W для M/G/1');
+    xlabel('\rho'); ylabel('W');
+    legend(arrayfun(@(x) ['C2b = ', num2str(x)], C2b_vals, 'UniformOutput', false));
+    
+    subplot(2,2,4);
+    for i = 1:length(C2b_vals)
+        plot(rho_vals, T(i,:)); hold on;
+    end
+    title('T для M/G/1');
+    xlabel('\rho'); ylabel('T');
+    legend(arrayfun(@(x) ['C2b = ', num2str(x)], C2b_vals, 'UniformOutput', false));
+end
+
+function plot_results(rho_vals, Nq, N, W, T, system_name)
+    figure;
+    subplot(2,2,1);
+    plot(rho_vals, Nq);
+    title(['N_q для ', system_name]);
     xlabel('\rho'); ylabel('N_q');
     
     subplot(2,2,2);
-    plot(rho_vals, N_MG1(i,:)); hold on;
-    title('N для M/G/1');
+    plot(rho_vals, N);
+    title(['N для ', system_name]);
     xlabel('\rho'); ylabel('N');
     
     subplot(2,2,3);
-    plot(rho_vals, W_MG1(i,:)); hold on;
-    title('W для M/G/1');
+    plot(rho_vals, W);
+    title(['W для ', system_name]);
     xlabel('\rho'); ylabel('W');
     
     subplot(2,2,4);
-    plot(rho_vals, T_MG1(i,:)); hold on;
-    title('T для M/G/1');
+    plot(rho_vals, T);
+    title(['T для ', system_name]);
     xlabel('\rho'); ylabel('T');
 end
 
-% M/D/1
-figure;
-subplot(2,2,1);
-plot(rho_vals, Nq_MD1);
-title('N_q для M/D/1');
-xlabel('\rho'); ylabel('N_q');
+%% Основная программа
+% Расчеты для M/G/1
+[Nq_MG1, N_MG1, W_MG1, T_MG1] = MG1_calculations(rho_vals, x_mean, C2b_vals);
+% Расчеты для M/D/1
+[Nq_MD1, N_MD1, W_MD1, T_MD1] = MD1_calculations(rho_vals, x_mean);
+% Расчеты для M/M/1
+[Nq_MM1, N_MM1, W_MM1, T_MM1] = MM1_calculations(rho_vals, x_mean);
 
-subplot(2,2,2);
-plot(rho_vals, N_MD1);
-title('N для M/D/1');
-xlabel('\rho'); ylabel('N');
+%% Построение графиков
+% Графики для M/G/1 с разными C2b на одном наборе графиков
+plot_results_MG1(rho_vals, Nq_MG1, N_MG1, W_MG1, T_MG1, C2b_vals);
 
-subplot(2,2,3);
-plot(rho_vals, W_MD1);
-title('W для M/D/1');
-xlabel('\rho'); ylabel('W');
+% Графики для M/D/1
+plot_results(rho_vals, Nq_MD1, N_MD1, W_MD1, T_MD1, 'M/D/1');
 
-subplot(2,2,4);
-plot(rho_vals, T_MD1);
-title('T для M/D/1');
-xlabel('\rho'); ylabel('T');
-
-% M/M/1
-figure;
-subplot(2,2,1);
-plot(rho_vals, Nq_MM1);
-title('N_q для M/M/1');
-xlabel('\rho'); ylabel('N_q');
-
-subplot(2,2,2);
-plot(rho_vals, N_MM1);
-title('N для M/M/1');
-xlabel('\rho'); ylabel('N');
-
-subplot(2,2,3);
-plot(rho_vals, W_MM1);
-title('W для M/M/1');
-xlabel('\rho'); ylabel('W');
-
-subplot(2,2,4);
-plot(rho_vals, T_MM1);
-title('T для M/M/1');
-xlabel('\rho'); ylabel('T');
+% Графики для M/M/1
+plot_results(rho_vals, Nq_MM1, N_MM1, W_MM1, T_MM1, 'M/M/1');
